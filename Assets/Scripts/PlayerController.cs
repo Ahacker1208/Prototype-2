4d0d0e6c9ab0180b8b88ed944af8afba31f18ac2
Ahacker1208 <97680603+Ahacker1208@users.noreply.github.com>
2022-10-21 12:44:26 -0400

    2.2 shooting cookie

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
     // Update is called once per frame
     void Update()
     {
@@ -23,6 +24,9 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-    
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+        }
     }
 }
